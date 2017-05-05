a2161f0db1e87bb359fcd53d454725460527ecb6
Alfio Parisi <alfioparisi.93@gmail.com>
2017-05-05 09:44:33 +0100

    webpack basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.min.js',
+    publicPath: '/dist'
+  }
+};
