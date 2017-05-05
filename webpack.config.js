65400821f64595eb892d232ac791ab51e56f78a0
Alfio Parisi <alfioparisi.93@gmail.com>
2017-05-05 10:06:03 +0100

    webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,60 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var extractPlugin = new ExtractTextPlugin({
+  filename: 'style.css'
+});
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.min.js',
-    publicPath: '/dist'
-  }
+    filename: 'bundle.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ]
 };
